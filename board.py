c4efb7eb45d0e70ee24b7c0ddb6d6343c22c3ae1
Mario Ramirez <mariosmac@Marios-Air.attlocal.net>
2022-04-18 13:31:15 -0400

    Print Board Functionality added

--- board.py
+++ board.py
@@ -1,2 +1,13 @@
 # This file contains the stages required to build a 2 player Tic Tac Toe game
 # By Mario Ramirez-Alvarenga
+# Defining functions 
+def print_board(boardVar):
+    for i in range(3):
+        print(boardVar[i])
+
+# Printing the board
+actualBoard = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
+print("Printing board...")
+print_board(actualBoard)
+
+
